081c69614c472d5d020131a5ceebb8cf1417a806
TaeHeyeon_Kim <thk98k@gmail.com>
2022-12-02 23:25:50 +0900

    feat(Dto): PostDto 클래스 구현

---
+++ src/main/java/com/coffee/app/spring/practice/member/dto/MemberPostDto.java
@@ -0,0 +1,10 @@
+package com.coffee.app.spring.practice.member.dto;
+
+import lombok.Getter;
+
+@Getter
+public class MemberPostDto {
+    private String name;
+    private String email;
+    private String phone;
+}
