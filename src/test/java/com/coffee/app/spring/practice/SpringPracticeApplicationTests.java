4b0af50da08b4208d4ab7bd67098e52228ac9026
TaeHeyeon_Kim <thk98k@gmail.com>
2022-12-02 22:45:53 +0900

    start spring project

---
+++ src/test/java/com/coffee/app/spring/practice/SpringPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffee.app.spring.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
