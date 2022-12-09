0eda15118d0ed7a369e654f6043b1ec5c7afcc47
TaeHeyeon_Kim <thk98k@gmail.com>
2022-12-09 13:35:17 +0900

    feat(member): MemberService 클래스 구현
    추후 비즈니스 로직 작성과 DB와 연결이 필요함

---
+++ src/main/java/com/coffee/app/spring/practice/member/service/MemberService.java
@@ -0,0 +1,53 @@
+package com.coffee.app.spring.practice.member.service;
+
+import com.coffee.app.spring.practice.member.entity.Member;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemberService {
+    public Member createMember(Member member) {
+        // TODO should business logic
+
+        // TODO member 객체는 나중에 DB에 저장 후 되돌려받는 로직으로 변경해야 한다.
+
+        Member createMember = member;
+        return createMember;
+    }
+
+    public Member updateMember(Member member) {
+        // TODO should business logic
+
+        // TODO member 객체는 나중에 DB에서 조회후 되돌려받는 로직으로 변경해야 한다.
+
+        Member updatedMember = member;
+        return updatedMember;
+    }
+
+    public Member findMember(long memberId) {
+        // TODO should business logic
+
+        // TODO member 객체는 나중에 DB에 조회하는 로직으로 변경해야 한다.
+        Member member = new Member(1L, "hgd1@naver.com", "홍길동2", "010-1111-1111");
+
+        return member;
+    }
+
+    public List<Member> findMembers() {
+        // TODO should business logic
+
+        // TODO member 객체는 나중에 DB에 조회하는 로직으로 변경해야 한다.
+
+        List<Member> members = new ArrayList<>();
+        Member member1 = new Member(1L, "hgd1@naver.com", "홍길동2", "010-1111-1111");
+        Member member2 = new Member(2L, "hgd2@naver.com", "홍길동2", "010-2222-2222");
+        members.add(member1);
+        members.add(member2);
+
+        return members;
+    }
+
+    public void deleteMember() {
+        // TODO should business logic
+    }
+}
