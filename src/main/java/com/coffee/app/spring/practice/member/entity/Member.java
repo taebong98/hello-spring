e4ad87a1e14d7722ed863ebdb8eae89007ba8036
TaeHeyeon_Kim <thk98k@gmail.com>
2022-12-09 13:34:20 +0900

    feat(member): member entity 클래스 생성

--- src/main/java/com/coffee/app/spring/practice/member/entity/Member.java
+++ src/main/java/com/coffee/app/spring/practice/member/entity/Member.java
@@ -1,10 +1,12 @@
 package com.coffee.app.spring.practice.member.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter @NoArgsConstructor
+@Getter @NoArgsConstructor @AllArgsConstructor
 public class Member {
+    private Long memberId;
     private String email;
     private String name;
     private String phone;
