9f2795a401b040eea0a47e5b39fee0db3d98c365
TaeHeyeon_Kim <thk98k@gmail.com>
2022-12-11 14:11:59 +0900

    feat(advice): ExceptionAdvice 클래스 정의

---
+++ src/main/java/com/coffee/app/spring/practice/advice/GlobalExceptionAdvice.java
@@ -0,0 +1,7 @@
+package com.coffee.app.spring.practice.advice;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionAdvice {
+}
