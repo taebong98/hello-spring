1152bc14dcf45cf93833b1caf2446323d5e5e2ce
TaeHeyeon_Kim <thk98k@gmail.com>
2022-12-11 19:24:11 +0900

    feat(advice): API 계층 예외처리 공통화 구현
    DTO 유효성 검증 예외처리, URI 변수로 넘어오는 값의 유효성 검증 예외처리 기능 구현

--- src/main/java/com/coffee/app/spring/practice/advice/GlobalExceptionAdvice.java
+++ src/main/java/com/coffee/app/spring/practice/advice/GlobalExceptionAdvice.java
@@ -2,30 +2,27 @@ package com.coffee.app.spring.practice.advice;
 
 import com.coffee.app.spring.practice.response.ErrorResponse;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import javax.validation.ConstraintViolationException;
+
 
 @RestControllerAdvice
 public class GlobalExceptionAdvice {
     @ExceptionHandler
-    public ResponseEntity handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-
-        final List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
-        List<ErrorResponse.FieldError> errors =
-                fieldErrors.stream()
-                        .map(error -> new ErrorResponse.FieldError(
-                                error.getField(),
-                                error.getRejectedValue(),
-                                error.getDefaultMessage()))
-                        .collect(Collectors.toList());
-
-        return new ResponseEntity(new ErrorResponse(errors), HttpStatus.BAD_REQUEST);
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        final ErrorResponse response = ErrorResponse.of(e.getBindingResult());
+        return response;
     }
 
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleConstraintViolationException(ConstraintViolationException e) {
+        final ErrorResponse response = ErrorResponse.of(e.getConstraintViolations());
+        return response;
+    }
 }
--- src/main/java/com/coffee/app/spring/practice/response/ErrorResponse.java
+++ src/main/java/com/coffee/app/spring/practice/response/ErrorResponse.java
@@ -1,20 +1,75 @@
 package com.coffee.app.spring.practice.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 
+import javax.validation.ConstraintViolation;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Getter
-@AllArgsConstructor
 public class ErrorResponse {
-    private List<FieldError> fieldErrors;
+    private List<FieldErrors> fieldErrors;
+    private List<ConstraintViolationError> violationErrors;
+
+    private ErrorResponse(List<FieldErrors> fieldErrors, List<ConstraintViolationError> violationErrors) {
+        this.fieldErrors = fieldErrors;
+        this.violationErrors = violationErrors;
+    }
+
+    public static ErrorResponse of(BindingResult bindingResult) {
+        return new ErrorResponse(FieldErrors.of(bindingResult), null);
+    }
+
+    public static ErrorResponse of(Set<ConstraintViolation<?>> violations) {
+        return new ErrorResponse(null, ConstraintViolationError.of(violations));
+    }
 
     @Getter
-    @AllArgsConstructor
-    public static class FieldError {
+    public static class FieldErrors {
         private String field;
         private Object rejectedValue;
         private String reason;
+
+        private FieldErrors(String field, Object rejectedValue, String reason) {
+            this.field = field;
+            this.rejectedValue = rejectedValue;
+            this.reason = reason;
+        }
+
+        public static List<FieldErrors> of(BindingResult bindingResult) {
+            final List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+
+            return fieldErrors.stream()
+                    .map(error -> new FieldErrors(
+                            error.getField(),
+                            error.getRejectedValue() == null ? "" : error.getRejectedValue().toString(),
+                            error.getDefaultMessage()
+                    )).collect(Collectors.toList());
+        }
+    }
+
+    @Getter
+    public static class ConstraintViolationError {
+        private String propertyPath;
+        private Object rejectedValue;
+        private String reason;
+
+        private ConstraintViolationError(String propertyPath, Object rejectedValue, String reason) {
+            this.propertyPath = propertyPath;
+            this.rejectedValue = rejectedValue;
+            this.reason = reason;
+        }
+
+        public static List<ConstraintViolationError> of (Set<ConstraintViolation<?>> constraintViolations) {
+            return constraintViolations.stream()
+                    .map(constraintViolation -> new ConstraintViolationError(
+                            constraintViolation.getPropertyPath().toString(),
+                            constraintViolation.getInvalidValue().toString(),
+                            constraintViolation.getMessage()
+                    )).collect(Collectors.toList());
+        }
     }
 }
