0b6ffaf9abd539c229aada57b1cbedae37a1b576
TaeHeyeon_Kim <thk98k@gmail.com>
2022-12-16 21:38:36 +0900

    fix(coffee): responseDto id멤버변수 추가

--- src/main/java/com/coffee/app/spring/practice/coffee/dto/CoffeeResponseDto.java
+++ src/main/java/com/coffee/app/spring/practice/coffee/dto/CoffeeResponseDto.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class CoffeeResponseDto {
+    private long coffeeId;
     private String korName;
     private String engName;
     private int price;
